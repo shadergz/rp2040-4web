59a2571582e53d1ad5cba542ef24404c9b9699fe
Correia <gabrielcorreia01012020@hotmail.com>
2024-04-25 20:45:16 -0300

    [ARM] - The LEDs connected to ports 18, 19, and 20 are now blinking

--- cortexm0+/server_pico.c
+++ cortexm0+/server_pico.c
@@ -1,15 +1,27 @@
 #include <pico/stdlib.h>
 
+static const uint8_t pinouts[] = {
+    18, 19, 20
+};
+
 int main() {
-    const uint LED_PIN = 14;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    for (uint8_t port = 0; port < 3; port++) {
+        gpio_init(pinouts[port]);
+        gpio_set_dir(pinouts[port], GPIO_OUT);
+    }
     volatile const uint loop = 1;
+    volatile uint high = 0;
 
     while (loop) {
-        gpio_put(LED_PIN, 1);
+        const uint8_t pin = pinouts[high];
+        ++high;
+
+        gpio_put(pin, 1);
         sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        gpio_put(pin, 0);
         sleep_ms(250);
+
+        if (high == 3)
+            high = 0;
     }
 }
